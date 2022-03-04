93d0e135f13284b9fc78b1984d65a3fd5424e35a
Karl Moran <karlmoran06@gmail.com>
2022-03-04 13:11:57 +0000

    Basic game structure, set up variables

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+# Player & Computer Map variables
+PLAYER_MAP = [[" "] * 8 for i in range(8)]
+COMPUTER_MAP = [[" "] * 8 for i in range(8)]
+PLAYER_GUESS_MAP = [[" "] * 8 for i in range(8)]
+COMPUTER_GUESS_MAP = [[" "] * 8 for i in range(8)]
+
+# Assigns letters to numbers that can be used for ship placements
+LETTERS_TO_NUMBERS = {
+    'A':0,
+    'B':1,
+    'C':2,
+    'D':3,
+    'E':4,
+    'F':5,
+    'G':6,
+    'H':7,
+}
