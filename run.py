81c3198ecbae273fe7f66b6723067a7eabf0f0e9
Karl Moran <karlmoran06@gmail.com>
2022-03-05 11:26:21 +0000

    Added a welcome_message function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 # Player & Computer Map variables
 PLAYER_MAP = [[" "] * 8 for i in range(8)]
@@ -7,13 +7,18 @@ PLAYER_GUESS_MAP = [[" "] * 8 for i in range(8)]
 COMPUTER_GUESS_MAP = [[" "] * 8 for i in range(8)]
 
 # Assigns letters to numbers that can be used for ship placements
-LETTERS_TO_NUMBERS = {
-    'A':0,
-    'B':1,
-    'C':2,
-    'D':3,
-    'E':4,
-    'F':5,
-    'G':6,
-    'H':7,
-}
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+def welcome_message():
+    """
+    The welcome_message function displays a welcome message
+    """
+    print("""\
+   ___             _       _        _              ___    _         _      _ __  
+  | _ )   __ _    | |_    | |_     | |     ___    / __|  | |_      (_)    | '_ \ 
+  | _ \  / _` |   |  _|   |  _|    | |    / -_)   \__ \  | ' \     | |    | .__/ 
+  |___/  \__,_|   _\__|   _\__|   _|_|_   \___|   |___/  |_||_|   _|_|_   |_|__  
+_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'      
+\
+""")
